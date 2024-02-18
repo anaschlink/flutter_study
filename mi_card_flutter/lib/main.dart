f277acf2cfcac5f3188e37d69326273e9125f30c
Ana Beatriz Schlink <abnschlink@gmail.com>
2024-02-18 00:51:01 -0300

    finish mi_card_flutter app

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_unnecessary_containers
+
 import 'package:flutter/material.dart';
 
 void main(){
@@ -10,11 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blueGrey,
         body: SafeArea(
             child: Column(
-             children: [
+              mainAxisAlignment: MainAxisAlignment.center,
+             children:[
                CircleAvatar(
                  radius: 50,
                  backgroundImage: AssetImage('images/profile_pic.jpg'),
@@ -33,8 +37,50 @@ class MyApp extends StatelessWidget {
                  fontFamily: 'Source Code Pro',
                  fontWeight: FontWeight.normal,
                  color: Colors.white,
-               ),)
-             ],
+               ),),
+               SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.grey,
+                ),
+               ),
+
+               Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                 child: Padding(
+                   padding: EdgeInsets.all(8.0),
+                   child: ListTile(
+                    leading: Icon(Icons.phone,
+                       color: Color.fromARGB(255, 9, 84, 122),),
+                       title: Text('+55 1234 5697',
+                       style: TextStyle(
+                        color: Colors.blueGrey,
+                        fontFamily:'Source Code Pro',
+                        fontSize: 20.0
+                       ),) 
+                   )
+                 ),
+               ),
+               Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child:Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading:
+                      Icon(Icons.email,
+                      color: Color.fromARGB(255, 9, 84, 122),),
+                      title: Text('angela@email.com',
+                      style: TextStyle(
+                        color: Colors.blueGrey,
+                        fontFamily:'Source Code Pro',
+                        fontSize: 20.0
+                      ),)
+                    ),
+                ),
+               )],
         ),)
       ),
     );
@@ -44,3 +90,4 @@ class MyApp extends StatelessWidget {
 
 
 }
+
